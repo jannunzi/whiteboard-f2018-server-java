6f54db3adc438858e47ad979b9b1227a403299fe
jannunzi <jannunzi@gmail.com>
2018-10-21 09:28:05 -0400

    added list and you tub widgets

---
+++ src/main/java/com/example/whiteboardfall2018serverjava/models/YouTubeWidget.java
@@ -0,0 +1,11 @@
+package com.example.whiteboardfall2018serverjava.models;
+
+public class YouTubeWidget extends Widget {
+	private String src;
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+}
